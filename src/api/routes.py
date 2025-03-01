a53d7bbd803e0638a9e607d212ef05d8e8acbc20
Roberto Torres Martínez <roberxiri@gmail.com>
2025-03-01 11:20:46 +0000

    First-Endpoint

--- src/api/routes.py
+++ src/api/routes.py
@@ -6,10 +6,34 @@ from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token
+from models import Smartphones
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+@api.route('/phones', methods=['POST'])
+def post_phones():
 
+    data = request.get_json()
+
+    exist = Smartphones.query.filter_by(modelo=data['modelo']).first()
+    if exist:
+        return jsonify({"msg": "This phone already exist in your list"}), 400
+    new_phone = Smartphones(
+        modelo = data['nombre'],
+        pantalla = data['pantalla'],
+        procesador = data['procesador'],
+        memoria_ram = data['memoria_ram'],
+        almacenamiento = data['almacenamiento'],
+        camara = data['camara'],
+        bateria = data['bateria'],
+        precio = data['precio'],
+        conectividad = data['conectividad'],
+        colores = data['colores'],
+        descripcion = data['descripcion'],
+    )
+    db.session.add(new_phone)
+    db.session.commit()
+    return jsonify({"msg": "Phone added"}), 200
